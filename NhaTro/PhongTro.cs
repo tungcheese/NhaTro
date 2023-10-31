71b8e4ce35b11233bc43dfe89769ba04201a2864
Lê Minh Huy <ngocnguyen.nnnc26@gmail.com>
2023-10-31 20:26:49 +0700

    alskdjalksdjqwd

--- NhaTro/PhongTro.cs
+++ NhaTro/PhongTro.cs
@@ -1,20 +1,37 @@
 ﻿public class PhongTro
 {
-    int sophong;
-    int dientich;
-    int giaphong;
-    string[] noithat;
-    uint songuoi;
-    int tien = 0;
+    int songuoi;
     string yeucau;
 
-    //
+    ///
+    int sophong;
+    int giaphong;
+    string[] noithat;
     bool gioitinh;
-    bool thucung;
-    bool loidirieng;
-    bool garage;
-    bool bancong;
-    bool camera;
+    int dientich;
+    ///
+    int tien = 0;
+    int tiendien;
+    int tiennuoc;
+    int tienrac;
+    public PhongTro(int sophong, string[] noithat, bool gioitinh, int dientich = 20)
+    {
+        this.sophong = sophong;
+        this.noithat = noithat;
+        this.gioitinh = gioitinh;
+        this.dientich = dientich;
+    }
 
-   
+    public void DienNuoc()
+    {
+        this.tienrac = 17000 * songuoi;
+        Random rd = new Random();
+        this.tiendien = (50000 + rd.Next(-10, 10) * 10000) * songuoi;
+        this.tiennuoc = 17750 * songuoi;
+    }
+
+    public void In()
+    {
+        Console.WriteLine();
+    }
 }
